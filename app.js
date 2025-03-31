1169a4addb36782dbbc919c1ab4cfdcbdb94cccd
Brandon Lin <brandonlin9716@gmail.com>
2025-03-31 13:08:16 -0700

    Serve index.ejs on / route

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 import express from 'express';
+import path from 'node:path';
+import { indexRouter } from './routes/indexRouter.js';
 
 const app = express();
+// looks for views in views folder
+const __dirname = path.resolve();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// routes
+app.use('/', indexRouter);
+
+// init server
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
