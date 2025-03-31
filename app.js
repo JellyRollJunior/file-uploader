b031174273728cb6aa7720e9f77112aa77029edc
Brandon Lin <brandonlin9716@gmail.com>
2025-03-31 14:40:35 -0700

    Setup import for local strategy

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 import express from 'express';
 import path from 'node:path';
+import dotenv from 'dotenv';
+import session from 'express-session';
+import passport from 'passport';
 import { indexRouter } from './routes/indexRouter.js';
 import { loginRouter } from './routes/loginRouter.js';
+dotenv.config();
 
 const app = express();
 // looks for views in views folder
 const __dirname = path.resolve();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// Passport
+import passportLocal from 'passport-local';
+import * as db from './db/queries.js';
+const LocalStrategy = passportLocal.Strategy;
+app.use(session({ secret: process.env.SECRET, resave: false, saveUninitialized: false }));
+app.use(passport.session()); // dependency for passport
+
+const local = new LocalStrategy(async (username, password, done) => {
+    
+})
+
 
 // routes
 app.use('/', indexRouter);
