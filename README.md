3f7ecad1fb3393b0fe9dedc5c309ed6a9a94c13a
Brandon Lin <brandonlin9716@gmail.com>
2025-04-01 15:40:26 -0700

    Make upload form accept a single file and log the file data

--- README.md
+++ README.md
@@ -13,6 +13,11 @@
 
 # todo:
 - multer middleware
+    - upload file location
+    - upload file name
+    - upload file size limit
+    - error handling
+
 
 - later
     - error handler middleware
--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -1,7 +1,14 @@
 import { Router } from 'express';
 import * as uploadController from '../controllers/uploadController.js';
 
+import multer from 'multer';
+const upload = multer();
+
 const uploadRouter = Router();
 uploadRouter.get('/', uploadController.getUpload);
+uploadRouter.post('/', upload.single('uploaded_file'), (req, res) => {
+    console.log(req.file);
+    res.redirect('/upload');
+})
 
 export { uploadRouter };
