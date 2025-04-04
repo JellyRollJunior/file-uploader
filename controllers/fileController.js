f41074d2b37724930435ae419ed4a0c3efd85f78
Brandon Lin <brandonlin9716@gmail.com>
2025-04-04 13:57:24 -0700

    Download file on clicking download button

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import * as db from '../db/queries.js';
 
 const getFile = async (req, res, next) => {
@@ -10,4 +11,16 @@ const getFile = async (req, res, next) => {
     }
 };
 
-export { getFile };
+const getFileDownload = async (req, res, next) => {
+    try {
+        const fileId = Number(req.params.fileId);
+        const file = await db.getFileById(fileId);
+        const __dirname = path.resolve();
+        const filePath = file.path;
+        res.download(`${__dirname}/${filePath}`, file.displayName);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export { getFile, getFileDownload };
--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -4,5 +4,6 @@ import * as fileController from '../controllers/fileController.js';
 const fileRouter = Router();
 
 fileRouter.get('/:fileId', fileController.getFile);
+fileRouter.get('/:fileId/download', fileController.getFileDownload);
 
 export { fileRouter };
