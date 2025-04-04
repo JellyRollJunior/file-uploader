470790f1411d0715c6e19a928524531712bcf32d
Brandon Lin <brandonlin9716@gmail.com>
2025-04-03 17:34:06 -0700

    Display file stats at /file/:fileId

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,5 +1,13 @@
-const getFile = (req, res) => {
-    res.render('file');
+import * as db from '../db/queries.js';
+
+const getFile = async (req, res, next) => {
+    try {
+        const fileId = Number(req.params.fileId);
+        const file = await db.getFileById(fileId);
+        res.render('file', { file });
+    } catch (error) {
+        next(error);
+    }
 };
 
 export { getFile };
